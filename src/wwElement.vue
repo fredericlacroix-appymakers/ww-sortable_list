363e77b140f7290d8354b6fe2e47357b94f6e7b0
fredericlacroix <frederic.lacroix@appymakers.com>
2025-12-15 15:55:21 +0100

    v2

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,11 +1,12 @@
 <template>
- <draggable
+  <draggable
     v-model="items"
     :group="content.group || 'default-group'"
     :itemKey="getItemKey"
     :disabled="isEditing"
     :animation="200"
     :style="{ ...$attrs.style, ...layoutStyle }"
+    @change="handleChange"
   >
     <template #item="{ element, index }">
       <div>
@@ -21,26 +22,46 @@
 import draggable from 'vuedraggable';
 
 export default {
-  components: {draggable},
+  components: { draggable },
   props: {
     content: { type: Object, required: true },
   },
   emits: ["trigger-event"],
   setup() {
-    return {layoutStyle: wwLib.useLayoutStyle() };
+    return { layoutStyle: wwLib.useLayoutStyle() };
   },
   computed: {
     items: {
       get() {
         return wwLib.wwCollection.getCollectionData(this.content.data) || [];
       },
-      set(value){
-        this.$emit("trigger-event", { name: "update:list",event:{ value }})
-      }
+      set(value) {
+        // Garde le set pour la réactivité locale
+        this.updateList(value);
       }
     }
+  },
+  methods: {
+    handleChange(evt) {
+      // Cette méthode est appelée par vuedraggable lors d'un changement
+      this.$nextTick(() => {
+        const currentItems = wwLib.wwCollection.getCollectionData(this.content.data) || [];
+        this.$emit("trigger-event", {
+          name: "update:list",
+          event: { 
+            value: currentItems,
+            change: evt // Info supplémentaire sur le changement
+          }
+        });
+      });
+    },
+    updateList(value) {
+      // Mise à jour de la collection
+      this.$emit("trigger-event", {
+        name: "update:list",
+        event: { value }
+      });
+    }
+  }
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
